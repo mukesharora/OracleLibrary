f186dcec37ba5babfaa7b8bf511008638d6fdf6c
Ali Koshar <ali.koshar@irissoftware.com>
2015-01-30 09:27:58 +0530

    for testing 2

--- OracleLibrary.TestApp/OracleClientApp.cs
+++ OracleLibrary.TestApp/OracleClientApp.cs
@@ -16,14 +16,12 @@ namespace OracleLibrary.TestApp
             var partsByPage = businessLayerObj.GetPartsByPage("partnumber", true, 2, 2);
             var partsByNumber = businessLayerObj.GetPartsByNumber("1", 2);
             var partsByName = businessLayerObj.GetPartsByName("a", 1);
-            var partsByDesc = businessLayerObj.GetPartsByDescription("b", 0);
+            var partsByDesc1 = businessLayerObj.GetPartsByDescription("b", 0);
 
-<<<<<<< HEAD
 
             // Comment 1
-=======
+
             // Comments for someTesting
->>>>>>> 4480c1dc9f7b95145da6f501f79f4c1350f75244
         }
     }
 }
